4430ff72b8f75e004ce402df4b21b1b05551fc7b
Anshu Kumar <anshu.ac.dv@gmail.com>
2023-04-03 11:06:23 +0530

    Now Comiit

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!Doctype html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<header>
+<body bgcolor="#E5E7E9">
+<nav>
+<div>
+<a href="index.php" style="text-decoration:none;" title="Home Page">
+<!-- <img style="float:left;padding:5px" src="Home.jpg" width="50" height="50"></img> -->
+<strong style="font-size:50px;">Employee Leave Management System</strong>
+</a>
+<a href="logout.php" ><img title="Logout" src="logout.png" width="40" height="40" style="float:right; padding:10px;"></img></a>
+</div>
+</nav>
+</header>
---
+++ includes/IncDBDetails.php
@@ -0,0 +1,6 @@
+<?php
+$dbServerName="localhost";
+$dbUserName="root";
+$dbPassword="";
+$dbName="empleave";
+$Connect = mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName);
